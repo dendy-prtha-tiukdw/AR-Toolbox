2af29d1a114288572ef7b51296b263ebf4cde5b0
dendy prtha <prtha.dendy@gmail.com>
2022-11-14 16:15:10 +0700

    upgrade android gradle version to 7.2.2 upgrade gradle build version to 7.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
-        classpath("com.android.tools.build:gradle:_")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("com.google.android.gms:oss-licenses-plugin:_")
     }
 }
